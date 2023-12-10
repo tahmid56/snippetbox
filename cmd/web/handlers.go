ec4834b27ee4f2e6fce2fdff6476df936b2e0d5d
Tahmid Akter <tahmidakter56@gmail.com>
2023-12-10 17:14:32 +0600

    structured the code base

---
+++ cmd/web/handlers.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request){
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID: %v", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodPost{
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a snippet"))
+}
