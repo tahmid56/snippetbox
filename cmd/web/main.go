1a9d8abc5e53789279874193c474d72473e1d682
Tahmid Akter <tahmidakter56@gmail.com>
2023-12-12 06:43:36 +0600

    db connection added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,16 +15,36 @@ type application struct {
 
 func main()  {
 	addr := flag.String("addr", ":4000", "HTTP network address") 
+	dsn := flag.String("dsn", "root:1234@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+
+	db, err := openDB(*dsn)
+
+	if err != nil { 
+		logger.Error(err.Error()) 
+		os.Exit(1)
+	}
 
 	app := &application{
 		logger: logger,
 	}
 
+	defer db.Close()
+
 	logger.Info("starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
+
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn) 
+	if err != nil {
+	return nil, err }
+	if err = db.Ping(); err != nil { return nil, err
+	}
+	return db, nil }
